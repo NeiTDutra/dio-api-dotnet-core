070aae049d03659745aef7a553d3ee43a61231a2
unknown <nei.thomass@gmail.com>
2021-05-20 09:49:45 -0300

    Enviando primeiros arquivos

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Course.api.Models;
+using Course.api.Models.Usuarios;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Course.api.Controllers
+{
+    [Route("api/v1/usuario")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        /// <summary>
+        /// teste
+        /// </summary>
+        /// <param name="loginViewModelInput">View model do login</param>
+        /// <returns>Retorna status ok, dados do usuario e o token em caso de exiistir</returns>
+        [SwaggerResponse(statusCode: 200, description: "Sucesso ao autenticar usuario", Type = typeof(LoginViewModelInput))]
+        [SwaggerResponse(statusCode: 400, description: "Campos obrigatorios", Type = typeof(ValidaCampoViewModelOutput))]
+        [SwaggerResponse(statusCode: 500, description: "Erro interno", Type = typeof(ErroGenericoViewModel))]
+        [HttpPost]
+        [Route("login")]
+        public IActionResult Logar(LoginViewModelInput loginViewModelInput) 
+        {
+            return Ok(loginViewModelInput);
+        }
+
+        [HttpPost]
+        [Route("registrar")]
+        public IActionResult Registrar(RegistroViewModelInput registroViewModelInput)
+        {
+            return Created("", registroViewModelInput);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Projeto API .NET Core
+
+***"Configuração da arquitetura back-end com .NET Core"***
+
+*Bootcamp TakeBlip Fullstack Developer - Digital Innovation One Inc.*
+
+## Descrição
+
+Desenvolvimento de uma api com .NET Core para cadastro de usuários e efetuação de login.
